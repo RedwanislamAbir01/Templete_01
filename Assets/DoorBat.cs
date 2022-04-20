35a8930c0b645b0cd259c3283dc90e6773469bd9
FPGL-Redwan01 <wahedifad@gmail.com>
2022-04-21 02:42:17 +0600

    0

--- Assets/DoorBat.cs
+++ Assets/DoorBat.cs
@@ -6,28 +6,23 @@ using UnityEngine;
 public enum eTypes
 {
     BatIcon , 
-    Joker, 
-    Car
+    Joker  
 }
 public class DoorBat : MonoBehaviour
 {
     public eTypes type;
-    public SpriteRenderer s;
-    public Sprite[] a;
+    public GameObject[] A;
     void Start()
     {
         if(type == eTypes.BatIcon)
         {
-            s.sprite = a[0];
+            A[0].gameObject.SetActive(true); A[01].gameObject.SetActive(false);
         }
         if (type == eTypes.Joker)
         {
-            s.sprite = a[01];
+            A[1].gameObject.SetActive(true); A[0].gameObject.SetActive(false);
         }
-        if (type == eTypes.Car)
-        {
-            s.sprite = a[02];
-        }
+   
     }
 
     // Update is called once per frame
