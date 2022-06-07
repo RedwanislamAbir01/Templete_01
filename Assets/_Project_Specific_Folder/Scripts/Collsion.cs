cf515794232a4e85da9881c51d771622fb12ee85
Redwan Islam <62832246+RedwanislamAbir01@users.noreply.github.com>
2022-06-07 16:14:45 +0600

    new cam setup

--- Assets/_Project_Specific_Folder/Scripts/Collsion.cs
+++ Assets/_Project_Specific_Folder/Scripts/Collsion.cs
@@ -336,37 +336,53 @@ public class Collsion : MonoBehaviour
         GameManager.Instance.BatMobile.transform.DOLocalMoveX(0, .3f);
        
         GameManager.Instance.BatMobile.transform.parent = GameManager.Instance.p.transform.GetChild(0);
-        Hero2.transform.DOJump(GameManager.Instance.BatMobile.transform.position, .5f, 1, .4f);
+        GameManager.Instance.p.speed = 0;
+        GameManager.Instance.p.MaxSpeed = 0;
+        GameManager.Instance.p.MinSpeed = 0;
+        GameManager.Instance.p.IncreazseMultiplier = 0;
+        Hero2.transform.DOJump(new Vector3(GameManager.Instance.BatMobile.transform.position.x+1,
+            GameManager.Instance.BatMobile.transform.position.y,
+            GameManager.Instance.BatMobile.transform.position.z
+
+            ), .5f, 1, .4f);
         yield return new WaitForSeconds(.1f);
         GameManager.Instance.p.enabled = true;
-        Hero1.transform.DOJump(GameManager.Instance.BatMobile.transform.position, .5f, 1, .5f).OnComplete(() => {
-            transform.GetComponent<Controller>().enabled = true;
+        Hero1.transform.DOJump(new Vector3(GameManager.Instance.BatMobile.transform.position.x+1,
+            GameManager.Instance.BatMobile.transform.position.y,
+            GameManager.Instance.BatMobile.transform.position.z ), .5f, 1, .5f).OnComplete(() => {
+            transform.GetComponent<CarController>().enabled = true;
             GameManager.Instance.BatMobile.transform.GetComponent<Animator>().Play("Close");
             Hero2.gameObject.SetActive(false);
             Hero1.gameObject.SetActive(false);
             GameManager.Instance.BatMobile.transform.DOLocalRotate(new Vector3(0, -90f, 0), .3f).OnComplete(() => {
 
-               
+                GameManager.Instance.BatMobile.transform.DOLocalMoveZ(GameManager.Instance.BatMobile.transform.localPosition.z - 13, .2f);
+                Camera.main.transform.DOLocalMoveZ(-9, .3f);
                 GameManager.Instance.BatMobile.transform.GetComponent<Animator>().Play("Driving");
-                GameManager.Instance.p.MaxSpeed = 9;
+                GameManager.Instance.p.MaxSpeed = 9; GameManager.Instance.p.speed =6; GameManager.Instance.p.IncreazseMultiplier = 5;
                 Camera.main.transform.GetChild(0).gameObject.SetActive(true);
             });
         });
         GameManager.Instance.ZoomEffect();
-        Camera.main.transform.DOLocalMoveZ(5f, 1.5f);
+       // Camera.main.transform.DOLocalMoveZ(5f, 1.5f);
 
         Hero1Model.GetComponent<Animator>().SetTrigger("Jump");
         Hero2Model.GetComponent<Animator>().SetTrigger("Jump");
 
+        yield return new WaitForSeconds(1f);
+        Hero1.transform.DOLocalMoveZ(12.07f, .3f); Hero2.transform.DOLocalMoveZ(12.07f, .3f);
+       
 
     }
     public IEnumerator OnExitRoutine()
     {
-        transform.GetComponent<Controller>().enabled = false;
+        transform.GetComponent<CarController>().enabled = false; 
+       // GameManager.Instance.BatMobile.transform.DOLocalMove(new Vector3(0, .21f, 0), .3f);
         Camera.main.transform.GetChild(0).gameObject.SetActive(false);
         //  GameManager.Instance.p.MaxSpeed -= SpeedIncreasAmmount;
         GameManager.Instance.p.speed = 0;
-        GameManager.Instance.p.MaxSpeed = 0; GameManager.Instance.p.MinSpeed = 0; GameManager.Instance.p.IncreazseMultiplier = 0;
+        GameManager.Instance.p.MaxSpeed = 0; GameManager.Instance.p.MinSpeed = 0; 
+        GameManager.Instance.p.IncreazseMultiplier = 0;
         GameManager.Instance.BatMobile.transform.DORotate(new Vector3(0, 54.4f, 0), .5f).OnComplete(() =>
         {
             GameManager.Instance.BatMobile.transform.GetComponent<Animator>().Play("Open");
@@ -378,43 +394,31 @@ public class Collsion : MonoBehaviour
 
         // GameManager.Instance.BatMobile.gameObject.SetActive(false);
         // });
-        yield return new WaitForSeconds(1.5f);
+        Camera.main.transform.DOLocalMoveZ(1.06f, .5f);
+        yield return new WaitForSeconds(1.5f); GameManager.Instance.BatMobile.transform.parent = null;
         Hero1.SetActive(true); Hero2.SetActive(true);
         //  GameManager.Instance.Fly.Stop();
-
-
-        Camera.main.transform.DOLocalMove(camStartPos, .2f);
-        Camera.main.transform.DOLocalRotate(camStartRot.eulerAngles, .2f);
-
-
         BatCape.transform.DOLocalRotate(StartCapeRot, .2f);
         BatCape.transform.DOLocalMove(StartCapePos, .01f);
-        Hero2Model.transform.DOLocalMove(Hero2ModelPos, .1f);
-        Hero1Model.GetComponent<Animator>().SetTrigger("Jump"); Hero2Model.GetComponent<Animator>().SetTrigger("Jump");
-        Target.transform.DOLocalMoveX(0, .1f);
-        transform.DOLocalMoveX(0, .1f);
-        GetComponent<Controller>().enabled = false;
+       
+        Hero1Model.GetComponent<Animator>().SetTrigger("Jump");
+        Hero2Model.GetComponent<Animator>().SetTrigger("Jump");
 
-        //Target.transform.GetChild(0).DOLocalMove(new Vector3(H1start.x, H1start.y, H1start.z + 10), .3f);
-        Target.transform.GetChild(01).transform.DOLocalMove(H2Start, .3f);
+        GetComponent<CarController>().enabled = false;
 
-        Hero1.transform.DOLocalJump(new Vector3(H1start.x, H1start.y, H1start.z + 10), .5f, 1, .3f);
-        GameManager.Instance.BatMobile.transform.DOLocalMove(new Vector3(0, .21f, 0), .3f);
-        transform.DOLocalMoveZ(5.6f, .3f);
-        Hero2.transform.DOLocalJump(new Vector3(H2Start.x, H2Start.y, H2Start.z + 10), .5f, 1, .3f).OnComplete(() =>
+
+        Hero1.transform.DOLocalJump(new Vector3(H1start.x, H1start.y, H1start.z + 40), .5f, 1, .3f);
+
+        transform.DOLocalMoveZ(5.6f, .3f); 
+        Hero2.transform.DOLocalJump(new Vector3(H2Start.x, H2Start.y, H2Start.z + 40), .5f, 1, .3f).OnComplete(() =>
         {
-            Hero1.transform.DOLocalMoveZ(0, .3f); Hero2.transform.DOLocalMoveZ(0, .3f);
-            Camera.main.transform.DOLocalMoveZ(-14.25f, .3f);
-            GameManager.Instance.BatMobile.transform.parent = null;
-            GameManager.Instance.p.speed = 1;
-            GameManager.Instance.p.MaxSpeed = 3;
+            Camera.main.transform.DOLocalMoveZ(25.66f, .5f);
+            //Hero1.transform.DOLocalMoveZ(0, .3f); Hero2.transform.DOLocalMoveZ(0, .3f);
+
+         
+            GameManager.Instance.p.speed = 2.5f;
+            GameManager.Instance.p.MaxSpeed = 2.5f;
             Hero1Model.GetComponent<Animator>().Play("Run"); Hero2Model.GetComponent<Animator>().Play("Run");
-
-            Shadow1.gameObject.SetActive(true); Shadow2.gameObject.SetActive(true);
-            //Connector.transform.DOLocalMove(new Vector3(-0.18f, .21f,10), 0f);
-            Connector.gameObject.SetActive(true);
-        
-
             Hero1.GetComponent<LookTowards>().enabled = true; Hero2.GetComponent<LookTowards>().enabled = true;
 
         }
